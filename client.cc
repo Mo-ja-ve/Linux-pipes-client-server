7170b399737b32acfc16ecc092681e93a0104910
Mojave <39601026+Mo-ja-ve@users.noreply.github.com>
2021-04-12 00:13:02 -0400

    Add files via upload

--- client.cc
+++ client.cc
@@ -15,13 +15,19 @@ using namespace std;
 //  type def structs for message passing scheme
 typedef enum {
      REGULAR,
-     COMMAND
+     EXIT,
+     STATUS,
+     TIME
 } msg_type_t;
 
 typedef struct msg {
+  int client_pid;
+}msg_initial_request_t;
+
+typedef struct msg_r {
      msg_type_t type;
      char message_text[MSG_LENGTH];
-}msg_t;
+}msg_client_request_t;
 
 //written by Andriy Kalinichenko
 int main()
@@ -41,13 +47,15 @@ int main()
     char * server_IN_fifo = "./server_np";
     //string to char conversion
     char client_send_fifo[100];
-    char client_recieve_fifo[100];
+    char client_recv_fifo[100];
     //char arrays to check if inputs are valid, will be tested against later
     char c_send[6] = {'s','e','n','d',':','\0'};
     char c_exit[5] = {'e','x','i','t','\0'};
+    char c_status[7] = {'s','t','a','t','u','s','\0'};
     strcpy(client_send_fifo,server_IN_fifo_s.c_str());
-    strcpy(client_recieve_fifo,client_IN_fifo_s.c_str());
-    msg_t msg;
+    strcpy(client_recv_fifo,client_IN_fifo_s.c_str());
+    msg_initial_request_t msg_init;
+    msg_client_request_t msg;
     msg_type_t typ;
 
     char client_msg[MSG_LENGTH];
@@ -58,22 +66,24 @@ int main()
 
     /* write PID to the FIFO */
     fd_server_NP = open(server_IN_fifo, O_WRONLY);
-    sprintf(buf, "%d", getpid());
-    if( write(fd_server_NP, buf, sizeof(buf)) < 0)
+    msg_init.client_pid = clientpid;
+    if( write(fd_server_NP, &msg_init, sizeof(msg_init)) < 0)
         fprintf(stderr, "Error\n");
     close(fd_server_NP);
 
+    while(1){
     // we send additional requests to the server
     cout<<endl<<"hello, type server request: ";
     cin >> client_str;
     int n = client_str.length();
     char temp_s[n+1];
     strcpy(temp_s, client_str.c_str());
-    temp_s[5] = '\0';
+    temp_s[n] = '\0';
 
     //checking to see if it's a send case
-    if(temp_s[0] == c_send[0] && temp_s[1] == c_send[1] && temp_s[2] == c_send[2] &&
-       temp_s[3] == c_send[3] && temp_s[4] == c_send[4] && temp_s[5] == c_send[5]){
+    if(n == 5){
+        if(temp_s[0] == c_send[0] && temp_s[1] == c_send[1] && temp_s[2] == c_send[2] &&
+        temp_s[3] == c_send[3] && temp_s[4] == c_send[4] && temp_s[5] == c_send[5]){
 
          //below we remove the send: portion of the string and send it to server, also set enum to REGULAR
          int c_string_length = client_str.length();
@@ -89,22 +99,48 @@ int main()
             fprintf(stderr, "Error\n");
 
          close(fd_client_send);
-
+       }
     }else{// checking to see if it's an exit case
-        if(temp_s[0] == c_exit[0] && temp_s[1] == c_exit[1] && temp_s[2] == c_exit[2] &&
+        if(n == 4){
+          if(temp_s[0] == c_exit[0] && temp_s[1] == c_exit[1] && temp_s[2] == c_exit[2] &&
            temp_s[3] == c_exit[3] && temp_s[4] == c_exit[4]){
 
           //exit command, we set enum and send it to server
-           typ = COMMAND;
+           typ = EXIT;
            msg.type = typ;
-           if( write(fd_client_send, &msg, sizeof(msg)) < 0);
+
+           fd_client_send = open(client_send_fifo, O_WRONLY);
+
+           if( write(fd_client_send, &msg, sizeof(msg)) < 0)
                fprintf(stderr, "Error\n");
 
+           close(fd_client_send);
            exit(EXIT_SUCCESS);
-       }else
-         cout<<endl<<"WARNING: INVALID INPUT";
-     }
+         }
+       }else{
+         if(n == 6){
+           if(temp_s[0] == c_status[0] && temp_s[1] == c_status[1] && temp_s[2] == c_status[2] &&
+           temp_s[3] == c_status[3] && temp_s[4] == c_status[4] && temp_s[5] == c_status[5] && temp_s[6] == c_status[6]){
 
+             typ = STATUS;
+             msg.type = typ;
+
+             fd_client_send = open(client_send_fifo, O_WRONLY);
+             if( write(fd_client_send, &msg, sizeof(msg)) < 0)
+                 fprintf(stderr, "Error\n");
+             close(fd_client_send);
+
+
+             fd_client_recv = open(client_recv_fifo, O_RDONLY);
+             while(read(fd_client_recv, &buf, MAX_BUF)>0){
+                  cout<<"number of clients: "<<buf<<endl;
+             }
+             close(fd_client_recv);
+         }
+       }else cout<<endl<<"WARNING: INVALID INPUT"; // no valid text was written
+   }
+ }
+}
     // strcpy(buf, client_str.c_str());
     // if( write(fd_server_IN, buf, sizeof(buf)) < 0)
     //     fprintf(stderr, "Error\n");
--- server.cc
+++ server.cc
@@ -6,37 +6,58 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <signal.h>
 
 #define MAX_BUF 1024
 #define MSG_LENGTH 100
+int client_count = 0;
 
 using namespace std;
 
 //  type def structs for message passing scheme
 typedef enum {
      REGULAR,
-     COMMAND
+     EXIT,
+     STATUS,
+     TIME
 } msg_type_t;
 
 typedef struct msg {
+  int client_pid;
+}msg_initial_request_t;
+
+typedef struct msg_r {
      msg_type_t type;
      char message_text[MSG_LENGTH];
-}msg_t;
+}msg_client_request_t;
 
+//  signal handler
+void handle_sig(int sig){
+  client_count--;
+  cout<<"client count decremented"<<endl;
+  cout<<"new client count: "<<client_count<<endl;
+}
 
 int main()
 {
-  int fd_server_IN, fd_client_recv, fd_client_send, msg_size;;
+  /*    SERVER PROCESS    */
+  int fd_server_IN, fd_client_recv, fd_client_send, msg_size;
   int fork_ret = 1;
   int clientpid;
   string tostring;
   char buf[MAX_BUF];
   char * server_IN_fifo = "./server_np";
   char client_send_fifo[100];
-  char client_recieve_fifo[100];
+  char client_recv_fifo[100];
   string client_recv_fifo_s;
   string client_send_fifo_s;
-  msg_t msg_s;
+  msg_initial_request_t msg_init;
+  msg_client_request_t msg_s;
+
+  //signal handling
+  struct sigaction act;
+  act.sa_handler = &handle_sig;
+  sigaction(SIGCHLD, &act, 0);
 
     /* open, read, and display the message from the FIFO */
     mkfifo(server_IN_fifo, 0666);
@@ -44,11 +65,13 @@ int main()
     while(1)
     {
         fd_server_IN = open(server_IN_fifo, O_RDONLY);
-        while(read(fd_server_IN, buf, MAX_BUF)>0)
+        while(read(fd_server_IN, &msg_init, 104)>0)
         {
-            printf("Received: %s\n", buf);
-            clientpid = atoi(buf);
+            printf("Received: %d\n", msg_init.client_pid);
+            clientpid = msg_init.client_pid;
             fork_ret = fork();
+            client_count++;
+            cout<<"number of clients: "<<client_count<<endl;
         }
         close(fd_server_IN);
 
@@ -58,20 +81,41 @@ int main()
           client_recv_fifo_s = "./" + tostring + "_recieve";
           client_send_fifo_s = "./" + tostring + "_send";
           strcpy(client_send_fifo, client_send_fifo_s.c_str());
+          strcpy(client_recv_fifo, client_recv_fifo_s.c_str());
+
           mkfifo(client_send_fifo, 0666);
+          mkfifo(client_recv_fifo, 0666);
 
           fd_client_send = open(client_send_fifo, O_RDONLY);
-
           while(read(fd_client_send, &msg_s, 104)>0){
             cout<<"message recieved"<<endl;
           }
           close(fd_client_send);
 
-          cout<<"message "<<msg_s.message_text<<endl;
-          cout<<"type: "<<msg_s.type<<endl;
-        }
-      }
-    }
+          if(msg_s.type == REGULAR){
+            cout<<"message: "<<msg_s.message_text<<endl;
+          }else{
+            if(msg_s.type == EXIT){
+              exit(EXIT_SUCCESS);
+          }else{
+            if(msg_s.type == STATUS){
+              cout<<"message type status recieved"<<endl;
+              cout<<"hello from server"<<endl;
+
+              sprintf(buf, "%d", client_count);
+              fd_client_recv = open(client_recv_fifo, O_WRONLY);
+              if( write(fd_client_recv, buf, sizeof(buf)) < 0)
+                cout<<"ERROR"<<endl;
+
+              close(fd_client_recv);
+
+              }
+            }
+          }
+        }//  end child while
+      }// end child case
+
+    }//  end of main while
 
     // fd_client_IN = open(client_IN_fifo, O_WRONLY);
     // sprintf(buf, "%d", getpid());
